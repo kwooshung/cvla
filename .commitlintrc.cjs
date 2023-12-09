58f6569a098c023328caf8297c440574be659f23
游水芦苇 <kwooshung@qq.com>
2023-12-09 21:45:00 +0800

    Forgot to use git hook and modified various configurations
    Forgot to use git hook, causing commitlint to be invalid
    1. The hook is now enabled!
    2. Conversion function, optimized!

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,6 +2,12 @@ const types = require('./scripts/ks-cvlar.types.cjs');
 const scopes = require('./scripts/ks-cvlar.scopes.cjs');
 const { ConvertToLintTypes, ConvertToLintScopes } = require('./dist/index.cjs');
 
+console.log('-------------------------------------');
+console.log({
+  'type-enum': [2, 'always', ConvertToLintTypes(types)],
+  'scope-enum': [2, 'always', ConvertToLintScopes(scopes)]
+});
+
 module.exports = {
   rules: {
     'type-enum': [2, 'always', ConvertToLintTypes(types)],
