5e733959941068392b38dd111a3e1b3edb9b86a1
游水芦苇 <kwooshung@qq.com>
2023-12-09 20:59:28 +0800

    Modify the conversion function of scopes
    Try to see if commitlint can enforce the standard

--- src/utils/convert/toLintScopes/index.ts
+++ src/utils/convert/toLintScopes/index.ts
@@ -2,9 +2,9 @@ import { ICommitScope } from '@/interface';
 
 /**
  * 转换为commitlint格式
- * @param {ICommitScope} types 类型
+ * @param {ICommitScope[]} scope 类型
  * @returns {string[]} 转换后的 Scopes
  */
-const toLintScopes = (types: ICommitScope): string[] => Object.keys(types);
+const toLintScopes = (scope: ICommitScope[]): string[] => scope.map((type: ICommitScope) => type.name);
 
 export default toLintScopes;
